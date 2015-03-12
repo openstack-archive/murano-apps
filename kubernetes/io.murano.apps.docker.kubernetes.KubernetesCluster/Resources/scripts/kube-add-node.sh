1390abc6112f93b029b2283329a0dfdcdc2881e0
Dmytro Dovbii <ddovbii@mirantis.com>
2015-03-13 00:28:12 +0200

    Replace kubecfg with kubectl
    Kubecfg is deprecated and it doesn't work on
    images with latest k8s
    
    Change-Id: I19f492709986af048e052617a7dde0baa6b75eb5

--- kubernetes/io.murano.apps.docker.kubernetes.KubernetesCluster/Resources/scripts/kube-add-node.sh
+++ kubernetes/io.murano.apps.docker.kubernetes.KubernetesCluster/Resources/scripts/kube-add-node.sh
@@ -7,6 +7,6 @@
 sed -i.bkp "s/%%NAME%%/$1/g" minion-node.json
 sed -i.bkp "s/%%IP%%/$2/g" minion-node.json
 
-/opt/bin/kubecfg -c minion-node.json create nodes
+/opt/bin/kubectl -f create minion-node.json
 
 /opt/bin/kubectl delete node 127.0.0.1
