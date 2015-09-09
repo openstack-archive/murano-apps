c1c75a21e3d390747290c4701ed5e47c69f54a78
Alexander Tivelkov <ativelkov@mirantis.com>
2015-09-09 19:18:34 +0300

    Added SugarCRM application package
    This package deploys the version 6.5.22 of SugarCRM CE.
    SugarCRM is a customer relationship management software. Its Community
    Edition is opensourced and is distributed under a GNU Affero General
    Public License.
    See README for more details.
    
    Change-Id: I88c41b9b1af59a3282403f505184296da03bb969

---
+++ SugarCRM/package/Resources/scripts/config_si.php
@@ -0,0 +1,68 @@
+<?php
+/*********************************************************************************
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo. If the display of the logo is not reasonably feasible for
+ * technical reasons, the Appropriate Legal Notices must display the words
+ * "Powered by SugarCRM".
+ ********************************************************************************/
+
+  $sugar_config_si = array (
+    'setup_db_host_name' => '%%DB_HOST%%',
+    'setup_db_database_name' => '%%DB_NAME%%',
+    'setup_db_drop_tables' => true,
+    'setup_db_create_database' => true,
+    'setup_site_admin_user_name' => '%%ADMIN_NAME%%',
+    'setup_site_admin_password' => '%%ADMIN_PASSWORD%%',
+    'setup_db_create_sugarsales_user' => false,
+    'setup_db_admin_user_name' => '%%DB_USER%%',
+    'setup_db_admin_password' => '%%DB_PASSWORD%%',
+    'setup_site_url' => 'http://%%HOST%%/Sugar',
+    'dbUSRData' => 'provide',
+    'setup_db_type' => 'mysql',
+    'setup_system_name' => 'SugarCRM',
+    'default_currency_iso4217' => 'USD',
+    'default_currency_name' => 'US Dollars',
+    'default_currency_significant_digits' => '2',
+    'default_date_format' => 'm/d/Y',
+    'default_time_format' => 'h:ia',
+    'default_decimal_seperator' => '.',
+    'default_export_charset' => 'UTF-8',
+    'default_language' => 'en_us',
+    'default_locale_name_format' => 's f l',
+    'default_number_grouping_seperator' => ',',
+    'export_delimiter' => ',',
+    'demoData' => '%%DEMO_DATA%%',
+    'setup_site_sugarbeet_anonymous_stats' => '1',
+    'setup_site_sugarbeet_automatic_checks' => '1',
+    'setup_site_specify_guid' => '0',
+    'setup_site_guid' => 'auto',
+    );
+?>
---
+++ SugarCRM/package/Resources/scripts/runSugarDeploy.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# SugarCRM Community Edition is a customer relationship management program developed by
+# SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License version 3 as published by the
+# Free Software Foundation with the addition of the following permission added
+# to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+# IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+# OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along with
+# this program; if not, see http://www.gnu.org/licenses or write to the Free
+# Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 USA.
+#
+# You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+# SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+#
+# The interactive user interfaces in modified source and object code versions
+# of this program must display Appropriate Legal Notices, as required under
+# Section 5 of the GNU Affero General Public License version 3.
+#
+# In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+# these Appropriate Legal Notices must retain the display of the "Powered by
+# SugarCRM" logo. If the display of the logo is not reasonably feasible for
+# technical reasons, the Appropriate Legal Notices must display the words
+# "Powered by SugarCRM".
+
+apt-get update
+apt-get -y -q install unzip
+apt-get -y -q install php5-mysql
+apt-get -y -q install mysql-client
+
+systemctl restart apache2
+
+wget -O sugar.zip https://www.dropbox.com/s/jlaabqamwj8hjcg/sugar.zip?dl=0
+sudo unzip -q sugar.zip -d /var/www/html/
+sudo chown -R www-data.www-data /var/www/html/Sugar
+sudo chmod -R 755 /var/www/html/Sugar
+
+sed -i.bkp "s/%%DB_HOST%%/$1/g; s/%%DB_NAME%%/$2/g; s/%%ADMIN_NAME%%/$3/g; s/%%ADMIN_PASSWORD%%/$4/g; s/%%DB_USER%%/$5/g; s/%%DB_PASSWORD%%/$6/g; s/%%HOST%%/$7/g; s/%%DEMO_DATA%%/$8/g" config_si.php
+
+cp config_si.php /var/www/html/Sugar/
+
+curl http://localhost/Sugar/install.php?goto=SilentInstall&cli=true
+
