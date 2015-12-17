9ae79d4f0d116e5e4d78f09c18ce760daaa04793
naresh thumma <naresht@vedams.com>
2015-12-17 07:12:43 -0800

    Provided REST API to OpenVPN server.

--- OpenVPN/package/Resources/scripts/deployOpenvpn.sh
+++ OpenVPN/package/Resources/scripts/deployOpenvpn.sh
@@ -18,6 +18,12 @@
 #$6 - gceNodesIp
 #$7 - publicServerPort
 #$8 - publicServerIp
+# $9 - instance name
+# $10 - environment name
+# $11 - OPENSTACK_IP
+# $12 - tenant
+# $13 - username
+# $14 - password
 
 openvpn_conf="/etc/os_openvpn/os_openvpn.conf"
 mkdir -p /etc/os_openvpn
@@ -33,11 +39,20 @@ echo "gcePassword=${5}" >> ${openvpn_conf}
 echo "gceNodesIp=${6}" >> ${openvpn_conf}
 echo "publicServerPort=${7}" >> ${openvpn_conf}
 echo "publicServerIp=${8}" >> ${openvpn_conf}
+echo " " >> ${openvpn_conf}
+echo "[INSTANCE]" >> ${openvpn_conf}
+echo "instanceName=${9}" >> ${openvpn_conf}
+echo "envName=${10}" >> ${openvpn_conf}
+echo "OPENSTACK_IP=${11}" >> ${openvpn_conf}
+echo "tenant=${12}" >> ${openvpn_conf}
+echo "username=${13}" >> ${openvpn_conf}
+echo "password=${14}" >> ${openvpn_conf}
 
 mkdir -p /opt/openvpn
 mkdir -p /opt/openvpn/templates
 cp openvpn/client.sh /opt/openvpn
 cp openvpn/server.sh /opt/openvpn
+cp openvpn/portDisable.py /opt/openvpn
 cp openvpn/templates/client.conf /opt/openvpn/templates
 cp openvpn/templates/down.sh /opt/openvpn/templates
 cp openvpn/templates/interfaces /opt/openvpn/templates
@@ -46,5 +61,5 @@ cp openvpn/templates/up.sh /opt/openvpn/templates
 
 bash /opt/openvpn/server.sh >> /tmp/openvpn-server.log
 bash /opt/openvpn/client.sh >> /tmp/openvpn-client.log
-
+python /opt/openvpn/portDisable.py >> /tmp/openvpn-port.log
 
---
+++ OpenVPN/package/Resources/scripts/openvpn/create-tap.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+set -e
+
+# Parsing input parameters from conf
+conf_file="/etc/os_openvpn/os_openvpn.conf"
+publicServerPort=$(awk -F "=" '/^publicServerPort/ {print $2}' $conf_file)
+publicServerIp=$(awk -F "=" '/^publicServerIp/ {print $2}' $conf_file)
+gceNodesIp=$1
+gceUserName="root"
+# ssh key Generating
+if [ ! -f ~/.ssh/id_rsa ]; then
+   ssh-keygen -t rsa -f  ~/.ssh/id_rsa -N ''
+fi
+
+
+# Client IPs storing in a array
+function create_client_array()
+{
+    while IFS=',' read -ra IP; do 
+        for i in "${IP[@]}"; do
+            CLIENTS_ARRAY+=("$i")
+        done
+    done <<< "$gceNodesIp"
+}
+
+# This func creates client names like new0, new1, new2...
+# Change name pattern if required. ex: pattern="client-"
+function create-client-name() {
+    pattern="client-"
+    count=0
+    while true
+    do
+       name="$pattern$count"
+       if [ ! -f /etc/openvpn/easy-rsa/keys/$name.crt ] && [ ! -f /etc/openvpn/easy-rsa/keys/$name.key ] 
+       then
+          CLIENT_NAME="$name"
+          break
+       else
+          ((count=count+1))
+          continue
+       fi
+    done
+}
+
+# Generating client certificates and copying 
+function gen_cert()
+{
+    clientname=$1
+    gceIp=$2
+    cd /etc/openvpn/easy-rsa
+    source ./vars
+    ./pkitool ${clientname}
+ 
+    
+    ssh-keygen -f "/root/.ssh/known_hosts" -R ${gceIp}
+    ssh-keyscan ${gceIp} >> ~/.ssh/known_hosts
+
+    ssh ${gceUserName}@${gceIp} uptime
+    ssh ${gceUserName}@${gceIp} sudo apt-get update
+    ssh ${gceUserName}@${gceIp} sudo apt-get install -y openvpn
+
+    scp /opt/openvpn/templates/client.conf /etc/openvpn/easy-rsa/keys/ca.crt /etc/openvpn/easy-rsa/keys/ta.key /etc/openvpn/easy-rsa/keys/${clientname}.crt /etc/openvpn/easy-rsa/keys/${clientname}.key ${gceUserName}@${gceIp}:
+
+    ssh ${gceUserName}@${gceIp} sed -ie s/SERVER_PORT/${publicServerPort}/ client.conf
+    ssh ${gceUserName}@${gceIp} sed -ie s/SERVER_IP/${publicServerIp}/ client.conf
+    ssh ${gceUserName}@${gceIp} sed -ie s/CLIENT/${clientname}/ client.conf
+    ssh ${gceUserName}@${gceIp} sudo cp client.conf ca.crt ta.key ${clientname}.crt ${clientname}.key /etc/openvpn
+
+    ssh ${gceUserName}@${gceIp} sudo service openvpn restart
+}
+
+
+create_client_array
+for ip in "${CLIENTS_ARRAY[@]}"
+do
+  
+   create-client-name
+   gen_cert $CLIENT_NAME $ip
+
+done
