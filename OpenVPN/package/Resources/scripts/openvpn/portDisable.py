5124bb227652b96155b6a022a8619bdf4775a09f
naresh thumma <naresht@vedams.com>
2015-12-18 06:00:04 -0800

    Removed hard coding of port numbers and Using exception handlings

--- OpenVPN/package/Resources/scripts/openvpn/portDisable.py
+++ OpenVPN/package/Resources/scripts/openvpn/portDisable.py
@@ -8,7 +8,7 @@ import logging
 logging.basicConfig(filename='/tmp/port-disable.log',level=logging.DEBUG)
 data_file = "/etc/os_openvpn/os_openvpn.conf"
 
-# parsing required parameters from data file
+# parsing required input parameters from data file
 config = ConfigParser.ConfigParser()
 config.read(data_file)
 config.sections()
@@ -29,34 +29,36 @@ neutron_service = "9696"
 nova_endPoint = "8774"
 
 # Requesting for auth token using OpenStack REST api
-
 def get_keystone_token():
     global token_id
     url = 'http://'+OPENSTACK_IP+':'+public_endPoint+'/v2.0/tokens'
     data = '{ "auth" : {"passwordCredentials": {"username": "'+username+'", "password": "'+password+'"}, "tenantName":"'+tenant+'"}}'
-    #print data
-    request = requests.post(url, data=data)
-    if request.status_code != 200:
+    try:
+        request = requests.post(url, data=data)
+    except Exception as e:
         logging.debug('Error while gettting auth token')
         logging.debug('%s',request.status_code)
-        sys.exit()
+        sys.exit(1)
     decode_response = (request.text.decode('utf-8'))
     string = str(decode_response)
     token_info = json.loads(string)
-    token_id = token_info["access"]["token"]["id"]
+    try:
+        token_id = token_info["access"]["token"]["id"]
+    except Exception as e:
+        sys.exit(1)
     logging.info('Received auth token')
 
 # Receiving and parsing instance details. Looking for instance id, port id, security group id, tenant id 
-
 def instance_details():
     global instance_info
     url =  'http://'+OPENSTACK_IP+':'+neutron_service+'/v2.0/ports.json'
     headers = {"X-Auth-Token": token_id}
-    request = requests.get(url, headers = headers)
-    if request.status_code != 200:
+    try:
+        request = requests.get(url, headers = headers)
+    except Exception as e:
         logging.debug('Error while gettting instance details')
         logging.debug('%s',request.status_code)
-        sys.exit()
+        sys.exit(1)
     decode_response = (request.text.decode('utf-8'))
     string = str(decode_response)
     ports_info = json.loads(string)
@@ -67,33 +69,38 @@ def instance_details():
 
 # Removing security group of an insatnce using REST api
 def remove_secgroup():
-    
-    instance_id = instance_info['device_id']
-    security_id = instance_info['security_groups'][0]
-    tenant_id = instance_info['tenant_id']
-    # print instance_id, security_id, tenant_id            
-
+    try:    
+        instance_id = instance_info['device_id']
+        security_id = instance_info['security_groups'][0]
+        tenant_id = instance_info['tenant_id']
+    except Exception as e:
+        sys.exit(1)
     url = 'http://'+OPENSTACK_IP+':'+nova_endPoint+'/v2.1/'+tenant_id+'/servers/'+instance_id+'/action'
     headers = { "Content-Type": "application/json", "X-Auth-Token": token_id}
     data = '{"removeSecurityGroup": {"name": "'+security_id+'"}}'
-    request = requests.post(url, headers = headers, data = data)
-    if request.status_code != 202:
+    try:
+        request = requests.post(url, headers = headers, data = data)
+    except Exception as e:    
         logging.debug('Error while removing secgroup of an instance')
         logging.debug('%s',request.status_code)
-        sys.exit()
+        sys.exit(1)
     logging.info('Security group has removed of an instance')
     
 # Disabling security port
 def port_disable():
-    port_id = instance_info['id']
+    try:
+        port_id = instance_info['id']
+    except Exception as e:
+        sys.exit(1)
     url = 'http://'+OPENSTACK_IP+':'+neutron_service+'/v2.0/ports/'+port_id+'.json'
     headers = { "Content-Type": "application/json", "X-Auth-Token": token_id}
-    data = '{"port": {"port_security_enabled": "False"}}'    
-    request = requests.put(url, headers = headers, data = data)
-    if request.status_code != 200:
+    data = '{"port": {"port_security_enabled": "False"}}'   
+    try: 
+        request = requests.put(url, headers = headers, data = data)
+    except Exception as e:
         logging.debug('Error while disabling securtiy port')
         logging.debug('%s',request.status_code)
-        sys.exit()
+        sys.exit(1)
     logging.info('Disabled scurtiy port')
 
 get_keystone_token()
