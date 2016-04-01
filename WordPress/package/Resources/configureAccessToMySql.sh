2fbbb04b200482931353e952ec1ee43b9f90a758
Dmytro Dovbii <ddovbii@mirantis.com>
2016-04-01 14:04:33 +0300

    [WordPress] Use simple software configuration
    Change-Id: If7e254ed03d040ce6348dc7094b57ccd35c55ca9
    Partially-Implements: bp refactor-apps-scripts

--- WordPress/package/Resources/configureAccessToMySql.sh
+++ WordPress/package/Resources/configureAccessToMySql.sh
@@ -12,9 +12,9 @@
 #  under the License.
 
 
-sudo sed -e "s/define('DB_NAME'.*$/define('DB_NAME', '$1');/" -i /var/www/html/wordpress/wp-config.php
-sudo sed -e "s/define('DB_USER'.*$/define('DB_USER', '$2');/" -i /var/www/html/wordpress/wp-config.php
-sudo sed -e "s/define('DB_PASSWORD'.*$/define('DB_PASSWORD', '$3');/" -i /var/www/html/wordpress/wp-config.php
-sudo sed -e "s/define('DB_HOST'.*$/define('DB_HOST', '$4');/" -i /var/www/html/wordpress/wp-config.php
+sudo sed -e "s/define('DB_NAME'.*$/define('DB_NAME', '%DATABASE%');/" -i /var/www/html/wordpress/wp-config.php
+sudo sed -e "s/define('DB_USER'.*$/define('DB_USER', '%USERNAME%');/" -i /var/www/html/wordpress/wp-config.php
+sudo sed -e "s/define('DB_PASSWORD'.*$/define('DB_PASSWORD', '%PASSWORD%');/" -i /var/www/html/wordpress/wp-config.php
+sudo sed -e "s/define('DB_HOST'.*$/define('DB_HOST', '%HOST%');/" -i /var/www/html/wordpress/wp-config.php
 
 sudo service apache2 restart
